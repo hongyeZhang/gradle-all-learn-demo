6a94495f366a325f138099c1cdbab59850583634
hongyeZhang <1034724029@qq.com>
2020-06-24 10:05:07 +0800

    06-24 commit

---
+++ gradle-java-demo/src/main/java/com/zhq/HelloTest.java
@@ -0,0 +1,12 @@
+package com.zhq;
+
+/**
+ * @Author ZHQ
+ * @Date 2020/6/24 9:58
+ */
+public class HelloTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
